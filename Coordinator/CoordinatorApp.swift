69b5626bcced2f18027239a9d78d02ffb11a8eaa
Mohammadreza Koohkan <mohamad_koohkan@icloud.com>
2022-07-18 10:57:08 +0300

    Initial Commit

---
+++ Coordinator/CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorApp.swift
+//  Coordinator
+//
+//  Created by Mohammad reza on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
